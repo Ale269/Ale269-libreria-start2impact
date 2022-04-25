d396c1fc1a7a5c09deaed780881dd5be2c131ccd
alessio manfredini <96541708+Ale269@users.noreply.github.com>
2022-04-25 17:12:08 +0200

    Update api.js

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -2,35 +2,12 @@ import axios from "axios";
 
 export default class GetBook {
     static getList = async function(url) {
-        /*return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject()
-            }else {
-                return response.json()
-            }
-            })
-        .then(data => {
-            return data.works;
-        }).catch(() => {
-            return "error";
-        })*/
-
         return axios.get(url)
         .then(response => response.data.works)
         .catch(error => "error");
     };
 
     static getDescription = async function(url) {
-        /*return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if(typeof data.description == "object" || data.description == undefined){
-                return "Description is not available for this book";
-            }
-            return data.description;
-        })*/
-
         return axios.get(url)
         .then((response) => {
             if(typeof response.data.description == "object" || response.data.description == undefined){
