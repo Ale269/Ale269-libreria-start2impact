40e70c98a6097a5f74658489ae921cd79d4ba56e
Alessio <alessio26manfredini@gmail.com>
2022-04-25 13:36:39 +0200

    Add axios

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -1,6 +1,8 @@
+import axios from "axios";
+
 export default class GetBook {
     static getList = async function(url) {
-        return fetch(url)
+        /*return fetch(url)
         .then(response => {
             if (!response.ok) {
                 return Promise.reject()
@@ -12,17 +14,29 @@ export default class GetBook {
             return data.works;
         }).catch(() => {
             return "error";
-        })
+        })*/
+
+        return axios.get(url)
+        .then(response => response.data.works)
+        .catch(error => "error");
     };
 
     static getDescription = async function(url) {
-        return fetch(url)
+        /*return fetch(url)
         .then(response => response.json())
         .then(data => {
             if(typeof data.description == "object" || data.description == undefined){
                 return "Description is not available for this book";
             }
             return data.description;
+        })*/
+
+        return axios.get(url)
+        .then((response) => {
+            if(typeof response.data.description == "object" || response.data.description == undefined){
+                return "Description is not available for this book";
+            }
+            return response.data.description;
         })
     }
 };
